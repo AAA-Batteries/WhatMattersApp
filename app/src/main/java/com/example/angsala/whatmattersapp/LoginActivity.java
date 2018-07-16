56d0c00b91796fb4f7b2bd355605e41e1fef2092
Angela Salacata <angsala@fb.com>
2018-07-16 15:26:44 -0700

    Started to inflate views in login screen

--- app/src/main/java/com/example/angsala/whatmattersapp/LoginActivity.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.example.angsala.whatmattersapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginUsername;
+    EditText loginPassword;
+    Button loginButton;
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         //test
+
+        loginUsername = (EditText) findViewById(R.id.loginUsername);
+        loginPassword = (EditText) findViewById(R.id.loginPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        createButton = (Button) findViewById(R.id.createButton);
     }
 }
