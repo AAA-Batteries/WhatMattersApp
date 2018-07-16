3b39627878fd370e3279d13ec76d6fbae2c4ce8f
Angela Salacata <angsala@fb.com>
2018-07-16 15:54:43 -0700

    updated gradle file

--- app/src/main/java/com/example/angsala/whatmattersapp/LoginActivity.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.angsala.whatmattersapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +23,24 @@ public class LoginActivity extends AppCompatActivity {
         loginPassword = (EditText) findViewById(R.id.loginPassword);
         loginButton = (Button) findViewById(R.id.loginButton);
         createButton = (Button) findViewById(R.id.createButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = loginUsername.getText().toString();
+                final String password = loginPassword.getText().toString();
+                //helper function to log in
+            }
+        });
+
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String newusername = loginUsername.getText().toString();
+                final String newpassword = loginPassword.getText().toString();
+                //helper function to make a new account
+            }
+        });
     }
 }
