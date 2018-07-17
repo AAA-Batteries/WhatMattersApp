3fb70d8d71b47d1ba6f59111ced16d7f089dfa23
ajkeyz <anjola@fb.com>
2018-07-17 10:05:52 -0700

    added a parse user class, parse adapter ,and dialog fragment

--- app/src/main/java/com/example/angsala/whatmattersapp/ContactActivity.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/ContactActivity.java
@@ -1,6 +1,7 @@
 package com.example.angsala.whatmattersapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,8 +11,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.angsala.whatmattersapp.model.User;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ContactActivity extends AppCompatActivity {
     User user;
@@ -63,11 +71,13 @@ public class ContactActivity extends AppCompatActivity {
 
     }
 
-   /* public void MyContacts(){
-        List<String> parseContacts = new List<String>();
+    public void MyContacts(){
+       List<String> parseContacts;
+        parseContacts = new List<String>();
+
+
         ParseQuery<ParseObject> pQuery = ParseQuery.getQuery("User");
         pQuery.findInBackground(new FindCallback<ParseObject>() {
-
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 if (e == null) {
@@ -88,7 +98,7 @@ public class ContactActivity extends AppCompatActivity {
 
 
 
-    }*/
+    }
 
 
 }
