42d12878023d671ebd4c5e4096e37a6c1edc265f
ajkeyz <anjola@fb.com>
2018-07-17 17:45:36 -0700

    add user functionality working

--- app/src/main/java/com/example/angsala/whatmattersapp/ContactFragment.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/ContactFragment.java
@@ -1,29 +1,75 @@
 package com.example.angsala.whatmattersapp;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 public class ContactFragment extends DialogFragment {
+
+    EditText newUsername;
+    private ContactFragmentListener listener;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (ContactFragmentListener) context;
+
+        } catch (ClassCastException e){
+           throw new ClassCastException(context.toString() + "must implement ContactFragmentListener");
+        }
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.addcontact, null);
+
+
+
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.addcontact, null))
+        builder.setView(view)
                 // Add action buttons
-                .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.addUser, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        // sign in the user ...
+
+
+                        String userText = newUsername.getText().toString();
+                        listener.applyTexts(userText);
+
+
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -31,7 +77,13 @@ public class ContactFragment extends DialogFragment {
                         ContactFragment.this.getDialog().cancel();
                     }
                 });
+        newUsername = view.findViewById(R.id.newUsername);
+
         return builder.create();
+    }
+
+    public interface  ContactFragmentListener{
+        void applyTexts(String userName);
     }
 
 }
