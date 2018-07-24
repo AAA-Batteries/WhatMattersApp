459b2d2c67439653240246716436a850fffd5228
ajkeyz <anjola@fb.com>
2018-07-23 17:06:34 -0700

    group adapter

--- app/src/main/java/com/example/angsala/whatmattersapp/GroupFragment.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/GroupFragment.java
@@ -1,10 +1,11 @@
 package com.example.angsala.whatmattersapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 
 
 public class GroupFragment extends Fragment {
+    ArrayList<Contacts> contacts;
+    RecyclerView rvContacts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,30 +27,35 @@ public class GroupFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_group, container, false);
     }
 
-    ArrayList<Contacts> contacts;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Lookup the recyclerview in activity layout
-        RecyclerView rvContacts = getView().findViewById(R.id.rvContacts);
 
-        // Initialize contacts
-        contacts = (ArrayList) ParseUser.getCurrentUser().get("contacts");
-        // Create adapter passing in the sample user data
-        ContactsAdapter adapter = new ContactsAdapter(contacts);
-        // Attach the adapter to the recyclerview to populate items
-        rvContacts.setAdapter(adapter);
-        // Set layout manager to position the items
-        rvContacts.setLayoutManager(new LinearLayoutManager(this.getContext()));
         // That's all!
 
         // First param is number of columns and second param is orientation i.e Vertical or Horizontal
-        StaggeredGridLayoutManager gridLayoutManager =
-                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+      //  StaggeredGridLayoutManager gridLayoutManager =
+
         // Attach the layout manager to the recycler view
-        rvContacts.setLayoutManager(gridLayoutManager);
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvContacts = view.findViewById(R.id.rvContacts);
+        // Initialize contacts
+        contacts = (ArrayList) ParseUser.getCurrentUser().get("contacts");
+        // Create adapter passing in the sample user data
+        GroupAdapter adapter = new GroupAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        rvContacts.setAdapter(adapter);
+
+    }
 }
