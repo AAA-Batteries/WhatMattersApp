7b698b8b1b5607f468b7b20db7760b75aeb76927
ajkeyz <anjola@fb.com>
2018-08-03 15:12:27 -0700

    image drawables for contacts session status, fixed the session status alignment

--- app/src/main/java/com/example/angsala/whatmattersapp/ProfileFragment.java
+++ app/src/main/java/com/example/angsala/whatmattersapp/ProfileFragment.java
@@ -9,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
